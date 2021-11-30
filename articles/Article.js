0972add0f8843ad0c1087d9fc3787fa4e894eac6
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-30 00:35:08 -0300

    Criando model Article com os relacionamentos

---
+++ articles/Article.js
@@ -0,0 +1,33 @@
+// Import sequelize
+const sequelize = require('sequelize');
+// Import connection
+const connection = require('../database/database');
+// Import category model
+const Category = require('../categories/Category');
+
+// Create article table
+const Article = connection.define('articles', {
+   title: {
+      type: sequelize.STRING,
+      allowNull: false,
+   },
+   slug: {
+      type: sequelize.STRING,
+      allowNull: false,   
+   }, 
+   body: {
+      type: sequelize.TEXT,
+      allowNull: false,
+   },
+});
+
+// One category has many articles (1 - N)
+Category.hasMany(Article);
+
+// An article belongs to a category (1 - 1)
+Article.belongsTo(Category);
+
+// Creating the table, but home it already exists, I do nothing.
+Article.sync({force: false});
+
+module.exports = Article;
