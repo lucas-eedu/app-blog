d7700db74312d4c7dc985c60b16bf8b5e3862c7c
Lucas Eduardo <lucas01.dev@gmail.com>
2021-11-29 22:49:30 -0300

    Chamando os meus domínios no arquivo index

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const bodyParser = require('body-parser');
 // Import connection with mysql
 const connection = require('./database/database');
 
+// Import Controllers
+const categoriesController = require('./categories/CategoriesController');
+const articlesController = require('./articles/ArticlesController');
+
 // Initializing view engine - EJS
 app.set('view engine', 'ejs');
 
@@ -26,6 +30,11 @@ connection
    .catch((error) => {
       console.error(error);
    })
+
+// Informing the application that I want to use the routes from the Controller file
+// Note that before calling routes I define a prefix (My route access prefix)
+app.use('/', categoriesController);
+app.use('/', articlesController);
 
 // Routes
 app.get('/', (req, res) => {
