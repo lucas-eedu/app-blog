1e97bd89743a6d9a2085c3346b00445b927e6568
Lucas Eduardo <lucas01.dev@gmail.com>
2021-12-07 14:58:59 -0300

    Criando rotas index, create e save

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,15 +1,41 @@
-// Import Express
+// Import express
 const express = require('express');
 // Express feature that allows you to create routes without the need to use the "app" variable
 const router = express.Router();
+// Import category model
+const Category = require('../categories/Category');
+// Import article model
+const Article = require('./Article');
+// Import slugify
+const slugify = require('slugify');
 
 // Articles Routes
-router.get('/articles', (req, res) => {
-   res.send('Rota de artigos');
+router.get('/admin/articles', (req, res) => {
+   Article.findAll().then(articles => {
+      res.render('admin/articles/index', {articles: articles});
+   });
 });
 
 router.get('/admin/articles/new', (req, res) => {
-   res.send('Rota para criar um novo artigo');
+   Category.findAll().then(categories => {
+      res.render('admin/articles/new', {categories: categories});
+   });
+});
+
+router.post('/articles/save', (req, res) => {
+   const title = req.body.title;
+   const body = req.body.body;
+   const category = req.body.category;
+
+   Article.create({
+      title: title,
+      slug: slugify(title),
+      body: body, 
+      category: category,
+      categoryId: category
+   }).then(() => {
+      res.redirect('/admin/articles');
+   });
 });
 
 module.exports = router;
