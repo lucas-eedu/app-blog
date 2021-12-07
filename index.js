bec2b43f0633b11104acf968271f5e09b6fb3bbc
Lucas Eduardo <lucas01.dev@gmail.com>
2021-12-07 18:55:05 -0300

    listando categorias no navbar

--- index.js
+++ index.js
@@ -46,7 +46,12 @@ app.get('/', (req, res) => {
       // Sorting articles from newest to oldest
       order: [['id', 'DESC']]
    }).then(articles => {
-      res.render('index', {articles: articles});
+      Category.findAll().then(categories => {
+         res.render('index', {
+            articles: articles,
+            categories: categories
+         });
+      });
    });
 });
 
@@ -58,7 +63,12 @@ app.get('/:slug', (req, res) => {
       }
    }).then(article => {
       if(article !== undefined) {
-         res.render('article', {article: article});
+         Category.findAll().then(categories => {
+            res.render('article', {
+               article: article,
+               categories: categories
+            });
+         });
       } else {
          res.redirect('/');
       }
