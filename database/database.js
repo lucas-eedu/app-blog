eec361444d76e4ab6ea3727b7f1002059e1ecc33
Lucas Eduardo <lucas01.dev@gmail.com>
2022-01-08 14:31:56 -0300

    Vercel Config

--- database/database.js
+++ database/database.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const connection = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
    host: process.env.DB_HOST,
    dialect: process.env.DB_DIALECT,
+   dialectModule: mysql2,
    timezone: process.env.DB_TIMEZONE
 });
 
