227a6a33030c5ba6961f5fb20448c2de5dafb69a
Lucas Eduardo <lucas01.dev@gmail.com>
2022-01-10 19:21:07 -0300

    CKEditor config

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,17 @@
 // Starting TinyMCE
-tinymce.init({
-   language: 'pt_BR',
-   selector: '#article',
-   plugins: [
-      'advlist autolink link image lists print preview hr searchreplace wordcount fullscreen insertdatetime media save table paste emoticons'
-   ],
-});
+// tinymce.init({
+//    language: 'pt_BR',
+//    selector: '#article',
+//    plugins: [
+//       'advlist autolink link image lists print preview hr searchreplace wordcount fullscreen insertdatetime media save table paste emoticons'
+//    ],
+// });
+ClassicEditor
+   .create( document.querySelector( '#editor' ) )
+   .catch( error => {
+      console.error( error );
+   }
+);
 
 // Opens a confirmation modal when asked to delete an item
 function confirmDelete(event, form) {
