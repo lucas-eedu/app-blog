88bbfe764de98b5262e220d53ab104cde188466c
Lucas Eduardo <lucas01.dev@gmail.com>
2022-01-10 19:01:26 -0300

    Reconfigurando tinymce

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 // Starting TinyMCE
-// tinymce.init({
-//    language: 'pt_BR',
-//    selector: '#article',
-//    plugins: [
-//       'advlist autolink link image lists print preview hr searchreplace wordcount fullscreen insertdatetime media save table paste emoticons'
-//    ],
-// });
+tinymce.init({
+   language: 'pt_BR',
+   selector: '#article',
+   plugins: [
+      'advlist autolink link image lists print preview hr searchreplace wordcount fullscreen insertdatetime media save table paste emoticons'
+   ],
+});
 
 // Opens a confirmation modal when asked to delete an item
 function confirmDelete(event, form) {
