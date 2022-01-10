b609fe626ae8025bfeb645722c72033ab305cb86
Lucas Eduardo <lucas01.dev@gmail.com>
2022-01-10 19:27:47 -0300

    Reconfigurando tinymce

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 // Starting TinyMCE
 tinymce.init({
-   // language: 'pt_BR',
+   language: 'pt_BR',
    selector: '#article',
    plugins: [
       'advlist autolink link image lists print preview hr searchreplace wordcount fullscreen insertdatetime media save table paste emoticons'
